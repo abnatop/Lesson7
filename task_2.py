feca5cbc8635576e2c23f4e78c1287f71e4162ac
abnatop <abnatop@gmail.com>
2020-12-22 21:26:08 +0900

    Задача 2. Development.

--- task_2.py
+++ task_2.py
@@ -10,4 +10,54 @@ V и H, соответственно.
 знания: реализовать абстрактные классы для основных классов проекта, проверить на
 практике работу декоратора @property .
 """
+from abc import ABC, abstractmethod
 
+class Clothes(ABC):  # Одежда
+    order_number = 1
+
+    def __init__(self, name):
+        self.name = name
+        self.order_number = Clothes.order_number
+        Clothes.order_number += 1
+
+    @property
+    def get_material(self):
+        return self._material_consumption
+
+class Coat(Clothes):  # Пальто, размер V = size
+    summary_material = 0.0
+
+    def __init__(self, name, size):
+        super().__init__(name)
+        self.size = size
+        self._material_consumption = round(self.size / 6.5 + 0.5, 2)
+        Coat.summary_material += self._material_consumption
+
+    def __str__(self):
+        return f'Заказ {self.order_number} - [{self.name}], размер {self.size}, ' + \
+               f'расход мат-ла {self.get_material}.'
+
+class Suit(Clothes):  # Костюм, рост H = height
+    summary_material = 0.0
+
+    def __init__(self, name, height):
+        super().__init__(name)
+        self.height = height
+        self._material_consumption = round(2 * self.height + 0.3, 2)
+        Suit.summary_material += self._material_consumption
+
+    def __str__(self):
+        return f'Заказ {self.order_number} - [ {self.name} ], рост {self.height}, ' + \
+               'расход мат-ла {self.get_material}.'
+
+d = [
+    Coat('Пальто1', size=10),
+    Coat('Пальто2', size=20),
+    Coat('Пальто3', size=30),
+    Suit('Костюм1', height=160),
+    Suit('Костюм2', height=180)
+]
+
+for i in d:
+    print(i)
+print('---')
